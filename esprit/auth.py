863ade4b3d7f5a9a24dfbe62339a31c2db3f5f87
TheLime03 <everpadd4@gmail.com>
2024-02-07 18:48:57 +0100

    rework auth

--- esprit/auth.py
+++ esprit/auth.py
@@ -1,100 +1,74 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
 class Auth:
-    """
-    A class used to represent an Authentication.
-
-    ...
-
-    Attributes
-    ----------
-    login_url : str
-        a formatted string that represents the login URL
-    session : requests.Session
-        a Session object from the requests library
-
-    Methods
-    -------
-    login(id: str, password: str):
-        Logs in to the website using the provided id and password.
-    """
-
-    def __init__(self, session=None):
+    def __init__(self, driver_path=None, driver=None, debug=False, headless=True):
         self.login_url = "https://esprit-tn.com/esponline/online/default.aspx"
-        self.session = session if session else requests.session()
+        self.debug = debug
+
+        # Set up Chrome options
+        chrome_options = Options()
+        if headless:
+            chrome_options.add_argument("--headless")
+
+        self.driver = driver if driver else webdriver.Chrome(
+            executable_path=driver_path, options=chrome_options)
 
     def login(self, id, password):
-        """
-        Logs in to the website using the provided id and password.
+        self.driver.get(self.login_url)
 
-        Parameters
-        ----------
-            id : str
-                the id to use for login
-            password : str
-                the password to use for login
+        # Fill in the ID
+        id_input = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="ContentPlaceHolder1_TextBox3"]'))
+        )
+        id_input.send_keys(id)
 
-        Returns
-        -------
-        requests.Session
-            the Session object used for the login. This can be used for subsequent requests.
-            Returns None if the login failed.
-        """
-        response = self.session.get(self.login_url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        viewstate = soup.find('input', {'id': '__VIEWSTATE'})['value']
-        viewstategenerator = soup.find(
-            'input', {'id': '__VIEWSTATEGENERATOR'})['value']
-        eventvalidation = soup.find(
-            'input', {'id': '__EVENTVALIDATION'})['value']
+        # Click the 'Suivant' button using XPath
+        suivant_button = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="ContentPlaceHolder1_Button3"]'))
+        )
+        suivant_button.click()
 
-        id_payload = {
-            '__EVENTTARGET': '',
-            '__EVENTARGUMENT': '',
-            '__VIEWSTATE': viewstate,
-            '__VIEWSTATEGENERATOR': viewstategenerator,
-            '__EVENTVALIDATION': eventvalidation,
-            'ctl00$ContentPlaceHolder1$TextBox1': '',
-            'ctl00$ContentPlaceHolder1$TextBox5': '',
-            'ctl00$ContentPlaceHolder1$TextBox6': '',
-            'ctl00$ContentPlaceHolder1$TextBox3': id,
-            'ctl00$ContentPlaceHolder1$Button3': 'Suivant',
-            'ctl00$ContentPlaceHolder1$TextBox4': '',
-            'ctl00$ContentPlaceHolder1$pass_parent': '',
-        }
+        # Fill in the password using XPath
+        password_input = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="ContentPlaceHolder1_TextBox7"]'))
+        )
+        password_input.send_keys(password)
 
-        response_id = self.session.post(self.login_url, data=id_payload)
+        # Click the 'Connexion' button using XPath
+        connexion_button = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="ContentPlaceHolder1_ButtonEtudiant"]'))
+        )
+        connexion_button.click()
 
-        response = self.session.get(self.login_url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        viewstate = soup.find('input', {'id': '__VIEWSTATE'})['value']
-        viewstategenerator = soup.find(
-            'input', {'id': '__VIEWSTATEGENERATOR'})['value']
-        eventvalidation = soup.find(
-            'input', {'id': '__EVENTVALIDATION'})['value']
-
-        password_payload = {
-            '__EVENTTARGET': '',
-            '__EVENTARGUMENT': '',
-            '__VIEWSTATE': viewstate,
-            '__VIEWSTATEGENERATOR': viewstategenerator,
-            '__EVENTVALIDATION': eventvalidation,
-            'ctl00$ContentPlaceHolder1$TextBox1': '',
-            'ctl00$ContentPlaceHolder1$TextBox5': '',
-            'ctl00$ContentPlaceHolder1$TextBox6': '',
-            'ctl00$ContentPlaceHolder1$TextBox7': password,
-            'ctl00$ContentPlaceHolder1$ButtonEtudiant': 'Connexion',
-            'ctl00$ContentPlaceHolder1$TextBox4': '',
-            'ctl00$ContentPlaceHolder1$pass_parent': '',
-        }
-
-        response_password = self.session.post(
-            self.login_url, data=password_payload)
-
-        if 'Vous pouvez consulter dans cet espace :' in response_password.text:
-            print("Login successful!")
-            return self.session
+        # Check if login was successful
+        if 'Vous pouvez consulter dans cet espace :' in self.driver.page_source:
+            print('''
+                  -----------------
+                  Login successful!
+                  -----------------
+                  ''')
+            return self.driver
         else:
-            print("Login failed.")
+            print('''
+                  -------------
+                  Login failed!
+                  -------------
+                  ''')
+
+
+# Code for debugging login
+if __name__ == "__main__":
+    auth = Auth(
+        driver_path="C:/custom/bins/chromedriver-win64/chromedriver.exe", debug=True)
+    id = "your_id"
+    password = "your_password"
+    session = auth.login(id, password)
