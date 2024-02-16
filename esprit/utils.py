11183862db2498865e36489669b0b7cccc736758
TheLime03 <everpadd4@gmail.com>
2024-02-16 20:35:18 +0100

    Refactor code to use string parameter instead of text parameter in BeautifulSoup find method

--- esprit/utils.py
+++ esprit/utils.py
@@ -20,9 +20,9 @@ class Utils:
         response = self.session.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Check if the <p> tag with the class "lead" and the text "Vous pouvez consulter dans cet espace :" exists
+        # Check if the <p> tag with the class "lead" and the string "Vous pouvez consulter dans cet espace :" exists
         p_tag = soup.find('p', class_='lead',
-                          text='Vous pouvez consulter dans cet espace :')
+                          string='Vous pouvez consulter dans cet espace :')
         if p_tag is None:
             print("The page does not contain the expected text.")
             return None
@@ -43,15 +43,14 @@ class Utils:
         response = self.session.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Check if the <p> tag with the class "lead" and the text "Vous pouvez consulter dans cet espace :" exists
+        # Check if the <p> tag with the class "lead" and the string "Vous pouvez consulter dans cet espace :" exists
         p_tag = soup.find('p', class_='lead',
-                          text='Vous pouvez consulter dans cet espace :')
+                          string='Vous pouvez consulter dans cet espace :')
         if p_tag is None:
             print("The page does not contain the expected text.")
             return None
 
-        span = soup.find(
-            'span', {'id': 'Label3', 'style': 'color:#CC0000;font-weight:bold;'})
+        span = soup.find('span', {'id': 'Label3'})
         if span is not None:
             return span.text.strip()
         else:
